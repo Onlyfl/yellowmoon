3116f8a048ab8344d589ecbc1861f28456259bd3
Иван Турьев <onlyfl@ukr.net>
2017-03-22 17:38:43 +0300

    22.03

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 $(function() {
     // code..
 })
+
+$(document).on('click','.portfolio_block_open', function() {
+
+       $('.portfolio_block_add').show("slow");
+	   $('.portfolio_block_open').toggleClass('opened');
+		
+
+    });
+	$(document).on('click','.opened', function() {
+
+       $('.portfolio_block_add').hide("slow");
+
+		
+
+    });
+	$(function(){
+		$(window).scroll(function() {
+
+		if($(this).scrollTop() >= 600) {
+			$('.header_nav').addClass('stickytop');
+			}
+			else{
+				$('.header_nav').removeClass('stickytop');
+				}
+			});
+		});
+		
+
+	
+	
